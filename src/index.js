41508f59ff70aaeeb42b798148174a222edd4820
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-08-04 20:38:57 -0700

    Completed Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const todoFormId = 'create-task-form'
+document.addEventListener('DOMContentLoaded', () => {
+  const todoForm = document.getElementById(todoFormId);
+  todoForm.addEventListener('submit', addToDo)
 });
+
+function addToDo (e) {
+  e.preventDefault();
+  const todoText = e.target.description.value
+  const taskList = document.getElementById('tasks')
+  
+  const newTask = document.createElement('li')
+  newTask.innerText = todoText;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = '[ X ]';
+  newTask.append(deleteButton);
+
+  deleteButton.addEventListener('click', e => {
+    const li = e.target.parentNode;
+    li.remove();
+     
+  });
+
+  taskList.append(newTask);
+
+  e.target.reset();
+}
